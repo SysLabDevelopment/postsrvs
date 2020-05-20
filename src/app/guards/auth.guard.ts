09016fc5c834685974c35ef6fdbc7d80b3e42d94
Тимофей <timkovik@yandex.ru>
2020-05-20 08:11:54 +0300

    ionicons only

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -5,6 +5,8 @@ import { Observable, BehaviorSubject } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { NavService } from '../services/nav.service';
 import { StateService } from '../services/state.service';
+import { Platform } from '@ionic/angular';
+import { AppVersion } from '@ionic-native/app-version/ngx';
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +17,9 @@ export class AuthGuard implements CanActivate {
     private nav_s: NavService,
     private state: StateService,
     private router: Router,
-    private auth: AuthService
+    private auth: AuthService,
+    private platform: Platform,
+    private appVersion: AppVersion
   ) {
 
   }
@@ -27,19 +31,15 @@ export class AuthGuard implements CanActivate {
 
     switch (url) {
       case 'courier':
-        console.log('next tab  3');
         this.nav_s.tabNav.next(3);
         break;
       case 'map':
-        console.log('next tab  2');
         this.nav_s.tabNav.next(2);
         break;
       case 'balance':
-        console.log('next tab  2');
         this.nav_s.tabNav.next(1);
         break;
       case 'login':
-        console.log('login_guard');
         if (this.state.g_state.getValue() == 'login') {
           return false;
         }
