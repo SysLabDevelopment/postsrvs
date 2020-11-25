78bb60327f811fad22d0c3db8a0b53a659508de1
timkovik <timkovik@yandex.ru>
2020-11-25 14:28:13 +0300

    feat: онбординг частички

--- src/app/components/part-delivered/part-delivered.component.ts
+++ src/app/components/part-delivered/part-delivered.component.ts
@@ -4,11 +4,12 @@ import { SysService } from 'src/app/services/sys.service';
 import { OrderService } from 'src/app/services/sys/order.service';
 import { ClientReceiptComponent } from '../client-receipt/client-receipt.component';
 import { GoodsTableComponent } from '../goods-table/goods-table.component';
+import { IntroJsService } from '@esfaenza/ngx-introjs';
 
 @Component({
   selector: 'app-part-delivered',
   templateUrl: './part-delivered.component.html',
-  styleUrls: ['./part-delivered.component.css'],
+  styleUrls: ['./part-delivered.component.scss'],
 })
 export class PartDeliveredComponent implements OnInit {
   public selectedPayment: any;
@@ -21,12 +22,29 @@ export class PartDeliveredComponent implements OnInit {
 
   @ViewChild('goodsTable') goodsTable: GoodsTableComponent;
   @ViewChild('receipt') receiptComponent: ClientReceiptComponent;
+  public introItems = {
+    Group: '5',
+    1: 'Ручное изменение принятых товаров',
+    2: 'Отсканируйте штрих-код товара на возврат',
+    3: 'Отредактируйте прочие детали',
+    4:'Закройте заказ'
+  };
 
   constructor(
     public modalController: ModalController,
     private order: OrderService,
-    private sys: SysService
-  ) { }
+    private sys: SysService,
+    public introService: IntroJsService
+  ) { 
+    this.introService.intro
+    .setOption('prevLabel', 'Назад')
+    .setOption('nextLabel', 'Далее')
+    .setOption('skipLabel', 'Пропустить')
+    .setOption('doneLabel', 'Завершить')
+    .setOption('scrollToElement', true)
+    .setOption('scrollTo', 'element');
+
+  }
 
   ngOnInit() { }
 
@@ -53,5 +71,8 @@ export class PartDeliveredComponent implements OnInit {
         this.sys.presentToast('Товар не найден', 'danger')
       }
     });
+  }
+  public showHelp() {
+    this.introService.start(null, '5');
   }
 }
